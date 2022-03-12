66f79107ec14be08925ebf3a630d5899811d258a
Jrevoredo42 <revoredojf@gmail.com>
2022-03-12 17:59:47 -0300

    rotas do projeto com comentarios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/listing';
+import Form from 'pages/forms';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-   <div>
-     <Navbar/>
-   </div> 
+    /*Inicializa estrutura de rotas */
+    <BrowserRouter>
 
+      {/*inicializa Navbar, declarando que ele irá aparecer em todas as paginas por ter sido iniciado antes
+      das rotas das paginas em si */}
+      <Navbar />
+
+      {/* Lista de rotas */}
+      <Routes>
+
+        {/*Rota de listagem (pagina princial) */}
+        <Route path="/" element={<Listing />} />
+
+        {/*Rota de formulario, que contem outra tag de rota com atributo path para mostrar que ela pode receber um codigo, 
+        junto do atributo element para a page de formulario, fazendo com que possam ter varias paginas de formularios, com itens
+        forms diferenciados pelo ID */}
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/forms/index.tsx
@@ -0,0 +1,7 @@
+function Forms(){
+    return(
+        <div>
+            <h1>page form </h1>
+        </div>
+    )
+}export default Forms
